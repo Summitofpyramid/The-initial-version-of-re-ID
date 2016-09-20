95bdbad2f169b8fafcd26aef0de885594608b384
Johnson Johnson <JohnsonJohnson@Johnsons-MacBook-Pro.local>
2016-09-20 14:33:48 -0400

    sift descriptor

---
+++ NewOpencProject/sift.hpp
@@ -0,0 +1,14 @@
+//
+//  sift.hpp
+//  NewOpencProject
+//
+//  Created by Johnson Johnson on 2016-09-08.
+//  Copyright © 2016 Johnson Johnson. All rights reserved.
+//
+
+#ifndef sift_hpp
+#define sift_hpp
+
+#include <stdio.h>
+
+#endif /* sift_hpp */
---
+++ transform.hpp
@@ -0,0 +1,14 @@
+//
+//  transform.hpp
+//  NewOpencProject
+//
+//  Created by Johnson Johnson on 2016-09-19.
+//  Copyright © 2016 Johnson Johnson. All rights reserved.
+//
+
+#ifndef transform_hpp
+#define transform_hpp
+
+#include <stdio.h>
+
+#endif /* transform_hpp */
